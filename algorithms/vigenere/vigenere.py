d7075d6b73b1833f5b9e575631a03d643fe7d348
Andrew Cotaj <acotaj@nd.edu>
2025-04-04 03:08:21 -0400

    Vignere basic algorithm implemented

--- algorithms/vigenere/vigenere.py
+++ algorithms/vigenere/vigenere.py
@@ -1,5 +1,44 @@
-def main():
-  pass
+def encode_vigenere(key, text):
+  key = key.upper()
+  result = []
+  key_index = 0
+  for char in text:
+    if char.isalpha():
+      shift = ord(key[key_index]) - ord('A')
+      if char.isupper():
+        result.append(chr((ord(char) - ord('A') + shift) % 26 + ord('A')))
+      else:
+        result.append(chr((ord(char) - ord('a') + shift) % 26 + ord('a')))
+      key_index = (key_index + 1) % len(key)
+    else:
+      result.append(char)
+  return ''.join(result)
 
-if __name__ == '__main__':
-  main()
+def decode_vigenere(key, text):
+  key = key.upper()
+  result = []
+  key_index = 0
+  for char in text:
+    if char.isalpha():
+      shift = ord(key[key_index]) - ord('A')
+      if char.isupper():
+        result.append(chr((ord(char) - ord('A') - shift) % 26 + ord('A')))
+      else:
+        result.append(chr((ord(char) - ord('a') - shift) % 26 + ord('a')))
+      key_index = (key_index + 1) % len(key)
+    else:
+      result.append(char)
+  return ''.join(result)
+
+
+# For testing the functions
+if __name__ == "__main__":
+  key = input("Enter the key: ")
+  text = input("Enter the text: ")
+  action = input("Do you want to encode or decode? (e/d): ")
+  if action.lower() == 'e':
+    print("Encoded text:", encode_vigenere(key, text))
+  elif action.lower() == 'd':
+    print("Decoded text:", decode_vigenere(key, text))
+  else:
+    print("Invalid action. Please enter 'e' for encode or 'd' for decode.")
